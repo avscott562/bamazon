a95380f2cf916e9ee9e24d9dfd4894a95c32bbba
Scottie <avscott@pacbell.net>
2019-10-16 18:45:33 -0700

    add products and table info to sql

--- bamazon.sql
+++ bamazon.sql
@@ -1,7 +1,7 @@
 -- Instructions
 
--- Drops the bamazon if it exists currently --
--- DROP DATABASE IF EXISTS bamazon;
+-- Drops the bamazon database if it exists currently --
+DROP DATABASE IF EXISTS bamazon;
 
 -- Create a MySQL Database called bamazon.
 CREATE DATABASE bamazon;
@@ -13,11 +13,17 @@ USE bamazon;
 CREATE TABLE products (
  -- The products table should have each of the following columns:
     -- item_id (unique id for each product)
+    item_id INT AUTO_INCREMENT NOT NULL,
     -- product_name (Name of product)
+    product_name VARCHAR(50) NULL,
     -- department_name
+    department_name VARCHAR(50) NULL,
     -- price (cost to customer)
+    price DECIMAL(4,2) NULL,
     -- stock_quantity (how much of the product is available in stores)
+    stock_quantity INTEGER NULL,
     -- PRIMARY_KEY(item_id);
+    PRIMARY_KEY(item_id);
 );
 
 
@@ -25,6 +31,6 @@ CREATE TABLE products (
 -- Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
 -- ### Alternative way to insert more than one row
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 10.30, 100), ("", "", 10.30, 100), ("", "", 10.30, 100);
+VALUES ("Bluetooth Speaker", "Electronics", 100.99, 100), ("Ballpoint Pens (2 pk)", "Office", 1.20, 100), ("Ink Cartridges (black)", "Office", 64.89, 100), ("Ink Cartridges (color)", "Office", 74.50, 100), ("Sweater", "Apparel & Accessories", 23.50, 100), ("Jeans", "Apparel & Accessories", 35.00, 100), ("Handbag", "Apparel & Accessories", 225.00, 100), ("Tennis Shoes", "Apparel & Accessories", 125.44, 100), ("Eyeshadow Palette", "Health & Beauty", 29.99, 100), ("Lipstick", "Health & Beauty", 16.99, 100);
 
 SELECT * FROM products
