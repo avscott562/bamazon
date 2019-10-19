b28085ca4d52c19f96a908aaecf0ecba6a92c33a
Scottie <avscott@pacbell.net>
2019-10-19 00:22:08 -0700

    add purchase function and inquirer

--- bamazon.sql
+++ bamazon.sql
@@ -19,11 +19,11 @@ CREATE TABLE products (
     -- department_name
     department_name VARCHAR(50) NULL,
     -- price (cost to customer)
-    price DECIMAL(4,2) NULL,
+    price DECIMAL(5,2) NULL,
     -- stock_quantity (how much of the product is available in stores)
     stock_quantity INTEGER NULL,
     -- PRIMARY_KEY(item_id);
-    PRIMARY_KEY(item_id);
+    PRIMARY KEY(item_id)
 );
 
 
@@ -33,4 +33,4 @@ CREATE TABLE products (
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Bluetooth Speaker", "Electronics", 100.99, 100), ("Ballpoint Pens (2 pk)", "Office", 1.20, 100), ("Ink Cartridges (black)", "Office", 64.89, 100), ("Ink Cartridges (color)", "Office", 74.50, 100), ("Sweater", "Apparel & Accessories", 23.50, 100), ("Jeans", "Apparel & Accessories", 35.00, 100), ("Handbag", "Apparel & Accessories", 225.00, 100), ("Tennis Shoes", "Apparel & Accessories", 125.44, 100), ("Eyeshadow Palette", "Health & Beauty", 29.99, 100), ("Lipstick", "Health & Beauty", 16.99, 100);
 
-SELECT * FROM products
+-- SELECT * FROM products
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,16 +2,58 @@ require("dotenv").config();
 let inquirer = require("inquirer");
 let mysql = require("mysql");
 
-console.log(process.env);
+console.log(process.env.DB_USER);
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: process.env.DB_USER,
+
+  // Your password
+  password: process.env.DB_PASS,
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  display();
+});
 
 // Instructions
 
 // Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+function display() {
+  console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      console.log(res.length);
+      purchase();
+    });
+}
+
 // The app should then prompt users with two messages.
 
+function purchase() {
+  // The first should ask them the ID of the product they would like to buy.
+  inquirer
+    .prompt({
+      name: "Item_id",
+      type: "input",
+      message: "What is the item id of the product you would like to purchase?"
+    })
+    .then(function(answer) {
+      console.log(answer.Item_id);
+    });
+    connection.end();
+}
 
-
-// The first should ask them the ID of the product they would like to buy.
 // The second message should ask how many units of the product they would like to buy.
 
 
@@ -29,94 +71,65 @@ console.log(process.env);
 
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
+  
 
-var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "",
-    database: "ice_creamDB"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    createProduct();
-  });
 
-  function createProduct() {
-    console.log("Inserting a new product...\n");
-    var query = connection.query(
-      "INSERT INTO products SET ?",
-      {
-        flavor: "Rocky Road",
-        price: 3.0,
-        quantity: 50
-      },
-      function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " product inserted!\n");
-        // Call updateProduct AFTER the INSERT completes
-        updateProduct();
-      }
-    );
+  // function createProduct() {
+  //   console.log("Inserting a new product...\n");
+  //   var query = connection.query(
+  //     "INSERT INTO products SET ?", 
+  //     {
+  //       flavor: "Rocky Road",
+  //       price: 3.0,
+  //       quantity: 50
+  //     },
+  //     function(err, res) {
+  //       if (err) throw err;
+  //       console.log(res.affectedRows + " product inserted!\n");
+  //       // Call updateProduct AFTER the INSERT completes
+  //       updateProduct();
+  //     }
+  //   );
   
-    // logs the actual query being run
-    console.log(query.sql);
-
-  function updateProduct() {
-    console.log("Updating all Rocky Road quantities...\n");
-    var query = connection.query(
-      "UPDATE products SET ? WHERE ?",
-      [
-        {
-          quantity: 100
-        },
-        {
-          flavor: "Rocky Road"
-        }
-      ],
-      function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " products updated!\n");
-        // Call deleteProduct AFTER the UPDATE completes
-        deleteProduct();
-      }
-    );
+  //   // logs the actual query being run
+  //   console.log(query.sql);
+  //   }
+  // function updateProduct() {
+  //   console.log("Updating all Rocky Road quantities...\n");
+  //   var query = connection.query(
+  //     "UPDATE products SET ? WHERE ?",
+  //     [
+  //       {
+  //         quantity: 100
+  //       },
+  //       {
+  //         flavor: "Rocky Road"
+  //       }
+  //     ],
+  //     function(err, res) {
+  //       if (err) throw err;
+  //       console.log(res.affectedRows + " products updated!\n");
+  //       // Call deleteProduct AFTER the UPDATE completes
+  //       deleteProduct();
+  //     }
+  //   );
   
-    // logs the actual query being run
-    console.log(query.sql);
-  }
+  //   // logs the actual query being run
+  //   console.log(query.sql);
+  // }
   
-  function deleteProduct() {
-    console.log("Deleting all strawberry icecream...\n");
-    connection.query(
-      "DELETE FROM products WHERE ?",
-      {
-        flavor: "strawberry"
-      },
-      function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " products deleted!\n");
-        // Call readProducts AFTER the DELETE completes
-        readProducts();
-      }
-    );
-  }
-  
-  function readProducts() {
-    console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      connection.end();
-    });
-  }
-  
+  // function deleteProduct() {
+  //   console.log("Deleting all strawberry icecream...\n");
+  //   connection.query(
+  //     "DELETE FROM products WHERE ?",
+  //     {
+  //       flavor: "strawberry"
+  //     },
+  //     function(err, res) {
+  //       if (err) throw err;
+  //       console.log(res.affectedRows + " products deleted!\n");
+  //       // Call readProducts AFTER the DELETE completes
+  //       readProducts();
+  //     }
+  //   );
+  // }
