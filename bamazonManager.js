06b111c9d775f33cfec4e7519c6e832c9b9ce3fa
Scottie <avscott@pacbell.net>
2019-10-20 01:49:08 -0700

    update addProduct function and switch statement case

--- bamazonManager.js
+++ bamazonManager.js
@@ -34,35 +34,100 @@ function menu() {
         name: "menuChoice",
         type: "list",
         message: "What function do you want to perform?",
-        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
     }).then(function(answer) {
         console.log(answer.menuChoice);
-        connection.end();
+        switch(answer.menuChoice) {
+            case "View Products for Sale": 
+                //run view products function;
+                console.log("view product worked");
+                break;
+            
+            case "View Low Inventory":
+                //run view low inventory function;
+                console.log("view low inventory worked");
+                break;
+            
+            case "Add to Inventory":
+                //run add inventory function;
+                console.log("add inventory worked");
+                break;
+            
+            case "Add New Product":
+                inquirer.prompt([
+                    {
+                        name: "productName",
+                        type: "input",
+                        message: "What is the name of the product?"
+                    },
+                    {
+                        name: "departmentName",
+                        type: "input",
+                        message: "What is the name of the department?"
+                    },
+                    {
+                        name: "price",
+                        type: "number",
+                        message: "What is the price of the product?",
+                        validate: function(value) {
+                            if(isNaN(value)==false) {
+                              return true;
+                            } else {
+                              return false;
+                            }        
+                        }
+                    },
+                    {
+                        name: "stock",
+                        type: "input",
+                        message: "How many are in stock?",
+                        validate: function(value) {
+                            if(isNaN(value)==false) {
+                              return true;
+                            } else {
+                              return false;
+                            }        
+                        }
+                    }
+                ]).then(function(answer) {
+                console.log("add product worked");
+                //run addProduct function;
+                addProduct(answer.productName, answer.departmentName, answer.price, answer.stock);
+                });
+                break;
+
+            case "Exit":
+                console.log("You are exiting."); 
+                connection.end();
+                break;
+        }
     });
 }
 
 
-
-// function createProduct() {
-  //   console.log("Inserting a new product...\n");
-  //   var query = connection.query(
-  //     "INSERT INTO products SET ?", 
-  //     {
-  //       flavor: "Rocky Road",
-  //       price: 3.0,
-  //       quantity: 50
-  //     },
-  //     function(err, res) {
-  //       if (err) throw err;
-  //       console.log(res.affectedRows + " product inserted!\n");
-  //       // Call updateProduct AFTER the INSERT completes
-  //       updateProduct();
-  //     }
-  //   );
+// allow the manager to add a completely new product to the store.
+function addProduct(product, dept, price, qty) {
+    console.log("Inserting a new product...\n");
+    var query = connection.query(
+      "INSERT INTO products SET ?", 
+      {
+        product_name: product,
+        department_name: dept,
+        price: price,
+        stock_quantity: qty
+      },
+      function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " product inserted!\n");
+        // Call updateProduct AFTER the INSERT completes
+        // updateProduct();
+      }
+    );
   
-  //   // logs the actual query being run
-  //   console.log(query.sql);
-  //   }
+    // logs the actual query being run
+    // console.log(query.sql);
+    connection.end();
+    }
   
   // function deleteProduct() {
   //   console.log("Deleting all strawberry icecream...\n");
@@ -85,4 +150,3 @@ function menu() {
 // If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
 // If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
 // If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
-// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
