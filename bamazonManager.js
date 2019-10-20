50a9ba2cb93fca0f39dba33689d9f670cb7a4b62
Scottie <avscott@pacbell.net>
2019-10-20 03:24:00 -0700

    add lowinventory function

--- bamazonManager.js
+++ bamazonManager.js
@@ -23,8 +23,13 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
 
-  //run menu function to see available menu options on screen.
-  menu();
+  console.log("Displaying all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+        if(err) throw err;
+        console.log(res);
+        //run menu function to see available menu options on screen.
+        menu();
+    });
 });
 
 //display menu options
@@ -44,13 +49,11 @@ function menu() {
                 break;
             
             case "View Low Inventory":
-                //run view low inventory function;
-                console.log("view low inventory worked");
+                lowInventory();
                 break;
             
             case "Add to Inventory":
-                //run add inventory function;
-                console.log("add inventory worked");
+                updateInventory();
                 break;
             
             case "Add New Product":
@@ -124,23 +127,91 @@ function addProduct() {
 }
 
 // If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
-function updateInventory(qty, amount, id) {
-    connection.query("UPDATE products SET ? WHERE ?",
-    [
-        {
-          stock_quantity: qty + amount
-        },
-        {
-          item_id: id
+function updateInventory() {
+
+    connection.query("SELECT * FROM products", function(err, res) {
+        if(err) throw err;
+        let products = res;
+        inquirer.prompt([
+            {
+                name: "itemId",
+                type: "number",
+                message: "What is the item_id of the product you want to update?",
+                validate: function(value) {
+                    if(isNaN(value)==false) {
+                      return true;
+                    } else {
+                      return false;
+                    }
+                  }
+            },
+            {
+                name: "qty",
+                type: "number",
+                message: "How much are you adding the inventory?",
+                validate: function(value) {
+                    if(isNaN(value)==false) {
+                      return true;
+                    } else {
+                      return false;
+                    }
+                  }
+            }
+        ]).then(function(answer) {
+            for (i=0; i<res.length; i++) {
+                if(answer.itemId === res[i].item_id) {
+                    console.log("got em!");
+                    connection.query("UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                        stock_quantity: res[i].stock_quantity + answer.qty
+                        },
+                        {
+                        item_id: res[i].item_id
+                        }
+                    ],
+                    function(err, res) {
+                        if (err) throw err;
+                        console.log(res.affectedRows + " products updated!\n");
+                        connection.end();
+                    })
+                }
+            }
+        });
+    })
+    
+}
+
+// If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, res) {
+        if(err) throw err;
+        switch(res.length) {
+            case 0:
+                console.log("We are fully stocked on all items!");
+                break;
+            
+            case 1:
+                console.log("There is 1 item that needs to be restocked.\n");
+                console.log(res);
+                break;
+            
+            default:
+                console.log("There are " + res.length + " items that need to be restocked.\n");
+                console.log(res);
+                break;
+
         }
-      ],
-      function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " products updated!\n");
-        // Call deleteProduct AFTER the UPDATE completes
-        // deleteProduct();
-      })
-}  
+        // if(res.length > 0) && (res.length < 2) {
+        //     console.log("These item(s) need to be restocked...\n");
+        //     console.log(res);
+        // } else {
+            
+        // }
+        connection.end();
+    })
+}
+
   // function deleteProduct() {
   //   console.log("Deleting all strawberry icecream...\n");
   //   connection.query(
@@ -160,4 +231,4 @@ function updateInventory(qty, amount, id) {
 
 
 // If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
-// If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+
